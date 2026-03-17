464405b8cbb79cbf15fb04f2d26f88abf76ed096
LukaszMuzzy <lukasz.mazur.wroclaw@gmail.com>
2026-03-17 17:03:49 +0000

    some prod adjustements

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '127.0.0.1,172.16.0.0/12,10.0.0.0/8');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
